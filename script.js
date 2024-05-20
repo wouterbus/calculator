8d7ba71621df11ba815a17ac173c308a7bbb33b7
wouterbus <59446222+wouterbus@users.noreply.github.com>
2024-05-20 18:19:52 -0300

    upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById('display')
+
+function appendToDisplay(input) {
+    display.value += input
+}
+
+function clearDisplay() {
+    display.value = ''
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    }
+    catch(error){
+        display.value = "Error"
+    }
+}
+
+function clearOne() {
+    display.value = display.value.slice(0, -1)
+} 
+
+
